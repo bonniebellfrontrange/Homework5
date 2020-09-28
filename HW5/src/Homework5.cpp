13c4ac6abdeeaa4811630f8c45776f2138066b2c
Bonnie Bell <cj6bonnie@gmail.com>
2020-09-28 08:42:40 -0600

    initial commit

---
+++ HW5/src/Homework5.cpp
@@ -0,0 +1,22 @@
+/*
+ * Homework5.cpp
+ *
+ *  Created on: Sep 27, 2020
+ *      Author: cj6bo
+ */
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+bool inDeckAlready(int newValue, int numberOfEntries, int deck[]) {
+	return false;
+}
+void printDeck(int deck[]) {
+}
+int main() {
+	srand (time(NULL));
+}
